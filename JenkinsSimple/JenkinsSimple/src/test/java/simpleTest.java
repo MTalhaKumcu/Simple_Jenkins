27849c3cb7b43a5a909cc1c39d91de9848c7f34e
Mtalhakumcu <mkumcu151@gmail.com>
2024-04-30 15:35:01 +0200

    simple project

---
+++ JenkinsSimple/JenkinsSimple/src/test/java/simpleTest.java
@@ -0,0 +1,56 @@
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class simpleTest {
+    @Test
+    void Test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test2() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test3() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test4() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test5() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test6() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test7() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test8() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test9() {
+        assertTrue(true);
+    }
+
+    @Test
+    void Test10() {
+        assertTrue(true);
+    }
+
+}
